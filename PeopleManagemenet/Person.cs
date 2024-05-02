106ddbdbc8a94682a1294bc55a5a039cd5f97541
Stone <stonemagar63@gmail.com>
2024-05-02 12:35:03 +0545

    Add Person.cs

---
+++ PeopleManagemenet/Person.cs
@@ -0,0 +1,13 @@
+class Person
+{
+    //Index,User Id,First Name,Last Name,Sex,Email,Phone,Date of birth,Job Title
+    public int index;
+    public string userId;
+    public string firstName;
+    public string lastName;
+    public string Gender;
+    public string email;
+    public string phone;
+    public DateTime dob;
+    public string jobTitle;
+}
